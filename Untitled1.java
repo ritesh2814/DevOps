650acf927d801e8b111906a77147c4151ebda7d2
ritesh2814 <ritesh282017@gmail.com>
2024-12-28 16:17:50 +0530

    add everthing

---
+++ Untitled1.java
@@ -0,0 +1,21 @@
+class  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		for(int i=0; i<n; i++){
+			System.out.println("he;llo");
+		}
+	}
+}
+
+class  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		for(int i=0; i<n; i++){
+			System.out.println("he;llo");
+		}
+	}
+}
