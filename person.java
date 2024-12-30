ee5282810431bda899dc7b1236656c835c50c337
ritesh2814 <ritesh282017@gmail.com>
2024-12-30 19:43:49 +0530

    git assignment

--- person.java
+++ person.java
@@ -4,36 +4,16 @@ class
 	{
 		System.out.println("pesrsono World!");
 		System.out.println("gfhhdjdj World!");
-		System.out.println("pesdhudkdrsono World!");
-		System.out.println("peddjdjdsrsono World!");
-		System.out.println("pesrsono World!");
-		System.out.println("Hello  World!");
-		System.out.println("pesrsono World!");
-		System.out.println("Hello  World!");
-		System.out.println("pesrsono World!");
+		
 				System.out.println("Hello  World!");
 		System.out.println("Hello  World!");
 		System.out.println("Hello  World!");
 		System.out.println("Hello  World!");
 		System.out.println("Hello  World!");
-		System.out.println("Hello  World!");
-		System.out.println("Hello  World!");
-		System.out.println("Hello  World!");
-		System.out.println("Hello  World!");
-		System.out.println("Hello  World!");
 
-sffsfvnllvxm .m
-	vncxkn nv. nvx
-	cknbl,nbl.cb
-
-	nbnvlbmljm;
-	hxkbnljmb;lc
 
 		System.out.println("pesrsono World!");
 
 	}
-Xkzlckzkvz
-zzmlxmv.lxv'cx
-fxnvlm;xv;/xv,v
-vlxv;,vl.,v.
+
 }
